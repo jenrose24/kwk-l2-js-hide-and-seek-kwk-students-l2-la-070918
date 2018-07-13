5041eee3e4840cb81cf987dfeeb20d71d731c910
jenrose24 <rosienifer@icloud.com>
2018-07-13 17:51:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ function nestedTarget(){
   return document.querySelector("#nested .target")
 }
 
-//function increaseRankBy(n){
-//const list = document.querySelectorAll (".ranked-list")
+function increaseRankBy(n){
+const list = document.querySelectorAll (".ranked-list")
 
-  //for (let i = 0; i < i++;) {
- // rankedlist[i].innerHTML = (i + 1)
- // }
-//}
+  for (let i = 0; i < i++;) {
+  rankedlist[i].innerHTML = (i + 1)
+  }
+}
 
 function deepestChild(){
 return document.querySelector('#grand-node div div div div')
